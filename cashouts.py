36b41d2e44cc2d98a83d3119aaaf4d4e2a0c07aa
nodeLogs <tsm.artem@gmail.com>
2023-04-20 12:53:48 +0300

    123

--- cashouts.py
+++ cashouts.py
@@ -7,9 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Замените значения переменных на ваши настройки
 MYSQL_HOST = os.environ["MYSQL_HOST"]
-MYSQL_PORT = int(os.environ["MYSQL_PORT"])  # Преобразовать значение порта в int
+MYSQL_PORT = int(os.environ["MYSQL_PORT"])
 MYSQL_USER = os.environ["MYSQL_USER"]
 MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
 MYSQL_DB_NAME = os.environ["MYSQL_DB_NAME"]
@@ -17,7 +16,6 @@ MYSQL_DB_NAME = os.environ["MYSQL_DB_NAME"]
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_CHANNEL_ID = os.environ["SLACK_CHANNEL_ID"]
 
-# Создание подключения к базе данных
 def create_db_connection():
     return mysql.connector.connect(
         host=MYSQL_HOST,
@@ -27,7 +25,6 @@ def create_db_connection():
         database=MYSQL_DB_NAME,
     )
 
-# Создание клиента Slack API
 slack_client = WebClient(token=SLACK_BOT_TOKEN)
 
 def get_status_text(status):
@@ -42,14 +39,13 @@ def get_status_text(status):
     else:
         return status
 
-def send_slack_message(transaction):
+def send_slack_message(transaction, conn):
     message_template = f"""*Merchant Cashout*
-
-:date:  *Created*: {transaction['created_at']}
-:link:  *Transaction ID*: {transaction['id']}
-:man_in_tuxedo:  *Merchant*: <https://cryptoprocessing-stage.corp.merehead.xyz/merchant/{transaction['owner_merchant_id']}/projects|Joy Casino>
-:slot_machine:  *Project*: <https://cryptoprocessing-stage.corp.merehead.xyz/merchant/{transaction['owner_merchant_id']}/project/{transaction['project_id']}/settings/details|Joy Casino Project>
-:money_with_wings:  *Amount*:  {transaction['amount']} {transaction['currency_network']}
+:date: Created: {transaction['created_at']}
+:link: Transaction link: {transaction['id']}
+:man_in_tuxedo: Merchant: {transaction['owner_merchant_id']}
+:slot_machine: Project: {transaction['project_id']}
+:money_with_wings: Amount:  {transaction['amount']} {transaction['currency_name']}
 
 {get_status_text(transaction['status'])}
 """
@@ -63,20 +59,23 @@ def send_slack_message(transaction):
         print(f"Error sending message: {e}")
 
 def update_slack_message(transaction, ts, conn):
-    merchant_name = get_merchant_name(transaction['owner_merchant_id'], conn)
-    message_template = f"""
-Status update for Transaction link: {transaction['id']}
-Status: {get_status_text(transaction['status'])}
+    message_template = f"""*Merchant Cashout*
+:date: Created: {transaction['created_at']}
+:link: Transaction link: {transaction['id']}
+:man_in_tuxedo: Merchant: {transaction['owner_merchant_id']}
+:slot_machine: Project: {transaction['project_id']}
+:money_with_wings: Amount:  {transaction['amount']} {transaction['currency_name']}
+
+{get_status_text(transaction['status'])}
 """
     try:
-        slack_client.chat_postMessage(
+        slack_client.chat_update(
             channel=SLACK_CHANNEL_ID,
-            thread_ts=ts,
+            ts=ts,
             text=message_template
         )
     except SlackApiError as e:
         print(f"Error updating message: {e}")
-
 
 def get_current_last_id():
     conn = create_db_connection()
@@ -110,28 +109,21 @@ def monitor_transactions():
         result = cursor.fetchall()
 
         for row in result:
-            if row['id'] not in message_ts_map:
-                ts = send_slack_message(row)
-                message_ts_map[row['id']] = ts
-                last_processed_id = row['id']
-            else:
-                ts = message_ts_map.get(row['id'])
-                if ts:
-                    update_slack_message(row, ts)
+          if last_processed_id is None or row['id'] > last_processed_id:
+              ts = send_slack_message(row, conn)
+              message_ts_map[row['id']] = ts
+          else:
+              ts = message_ts_map.get(row['id'])
+              if ts:
+                  update_slack_message(row, ts, conn)
 
-        # Добавим код для проверки статусов сообщений, отправленных после запуска бота
-        for transaction_id, ts in message_ts_map.items():
-            query = f"SELECT * FROM project_withdrawal_crypto_transactions WHERE id = {transaction_id}"
-            cursor.execute(query)
-            row = cursor.fetchone()
-
-            if row:
-                update_slack_message(row, ts, conn)
+        if result:
+            last_processed_id = max(row['id'] for row in result)
 
         cursor.close()
         conn.close()
-
-        time.sleep(5)
+        time.sleep(5)  # Check for new transactions every minute
 
 if __name__ == "__main__":
     monitor_transactions()
+
